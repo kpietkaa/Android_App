0de40a4906a561b760361dec4f50915291c91936
kpietkaa <kpietkaa@gmail.com>
2016-04-15 20:10:03 +0200

    Add listLights method

--- ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
+++ ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
@@ -1,5 +1,12 @@
 package com.kaz.zpi_kaz_app.tools;
 
+import org.json.JSONArray;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 
 public class LIFX {
 
@@ -17,5 +24,32 @@ public class LIFX {
     public LIFX()
     {
         apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
+    }
+
+    public JSONArray listLights()
+    {
+        try
+        {
+            URL adres = new URL("https://api.lifx.com/v1/lights/all");
+            HttpURLConnection polaczenie = (HttpURLConnection) adres.openConnection();
+            polaczenie.setRequestProperty("Authorization", " Bearer " + apiKey);
+            polaczenie.setRequestMethod("GET");
+
+            BufferedReader wejscie = new BufferedReader(new InputStreamReader((polaczenie.getInputStream())));
+            StringBuilder odpowiedz = new StringBuilder();
+
+            for (String json; (json = wejscie.readLine()) != null;)
+            {
+                odpowiedz.append(json);
+            }
+            wejscie.close();
+
+            return new JSONArray(odpowiedz.toString());
+        }
+        catch (Exception wyjatek)
+        {
+            wyjatek.printStackTrace();
+        }
+        return new JSONArray();
     }
 }
