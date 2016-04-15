1556cd50a107c54f3123c060849a9c04e427fecd
kpietkaa <kpietkaa@gmail.com>
2016-04-15 19:11:36 +0200

    Add LIFX class & constructor

---
+++ ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
@@ -0,0 +1,21 @@
+package com.kaz.zpi_kaz_app.tools;
+
+
+public class LIFX {
+
+    String apiKey;
+
+    // Pobierania tokena potrzebnego do autoryzacji przy polczeniu z zarkowka - na przysloszc
+    /*
+    public LIFX(String klucz)
+    {
+        apiKey = klucz;
+    }
+    */
+
+    // Na nasze poczatkowe potrzeby zapisuje tu na stale
+    public LIFX()
+    {
+        apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
+    }
+}
