0ef307def64bbdccd8151b315f39a7306d1009af
kpietkaa <kpietkaa@gmail.com>
2016-04-16 19:44:03 +0200

    TO jest dobrze, tylko glupi Android jest glupi. :facepalm: #tylkoIOS #teamApple

--- ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
+++ ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
@@ -1,16 +1,24 @@
 package com.kaz.zpi_kaz_app.tools;
 
+import android.widget.TextView;
+
+import com.kaz.zpi_kaz_app.MainActivity;
+import com.kaz.zpi_kaz_app.R;
+
 import org.json.JSONArray;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
+
 import javax.net.ssl.HttpsURLConnection;
 
 public class LIFX {
 
     String apiKey;
+    String str = "[{\"name\":\"name1\",\"url\":\"url1\"},{\"name\":\"name2\",\"url\":\"url2\"}]";
 
     // Pobierania tokena potrzebnego do autoryzacji przy polczeniu z zarkowka - na przysloszc
     /*
@@ -21,35 +29,36 @@ public class LIFX {
     */
 
     // Na nasze poczatkowe potrzeby zapisuje tu na stale
-    public LIFX()
-    {
-        apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
-    }
+//    public LIFX()
+//    {
+//        apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
+//    }
 
     public JSONArray listLights()
     {
+        String apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
+
         try
         {
             URL adres = new URL("https://api.lifx.com/v1/lights/all");
-            HttpURLConnection polaczenie = (HttpURLConnection) adres.openConnection();
-            polaczenie.setRequestProperty("Authorization", " Bearer " + apiKey);
+            HttpsURLConnection polaczenie = (HttpsURLConnection) adres.openConnection();
+            polaczenie.setRequestProperty("Authorization", "Bearer " + apiKey);
             polaczenie.setRequestMethod("GET");
 
-            BufferedReader wejscie = new BufferedReader(new InputStreamReader((polaczenie.getInputStream())));
+            BufferedReader wejscie = new BufferedReader(new InputStreamReader(polaczenie.getInputStream()));
             StringBuilder odpowiedz = new StringBuilder();
 
-            for (String json; (json = wejscie.readLine()) != null;)
-            {
+            String json;
+            while ((json = wejscie.readLine()) != null)
                 odpowiedz.append(json);
-            }
             wejscie.close();
-
             return new JSONArray(odpowiedz.toString());
         }
-        catch (Exception wyjatek)
+        catch (Exception e)
         {
-            wyjatek.printStackTrace();
+            e.printStackTrace();
         }
+
         return new JSONArray();
     }
 }
