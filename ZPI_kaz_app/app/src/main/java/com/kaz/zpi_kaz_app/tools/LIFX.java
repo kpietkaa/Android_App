cccf59dc438a36f1c16482dbd2f34d1fdc9b033e
kpietkaa <kpietkaa@gmail.com>
2016-04-17 09:48:20 +0200

    Clean LIFX class

--- ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
+++ ZPI_kaz_app/app/src/main/java/com/kaz/zpi_kaz_app/tools/LIFX.java
@@ -1,49 +1,35 @@
+/*
+*   Polaczenie synchroniczne z zarowka,
+*   po prostu nie dziala :)
+*/
 package com.kaz.zpi_kaz_app.tools;
-
-import android.widget.TextView;
-
-import com.kaz.zpi_kaz_app.MainActivity;
-import com.kaz.zpi_kaz_app.R;
 
 import org.json.JSONArray;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Objects;
-
 import javax.net.ssl.HttpsURLConnection;
 
 public class LIFX {
 
     String apiKey;
-    String str = "[{\"name\":\"name1\",\"url\":\"url1\"},{\"name\":\"name2\",\"url\":\"url2\"}]";
-
-    // Pobierania tokena potrzebnego do autoryzacji przy polczeniu z zarkowka - na przysloszc
-    /*
-    public LIFX(String klucz)
-    {
-        apiKey = klucz;
-    }
-    */
 
     // Na nasze poczatkowe potrzeby zapisuje tu na stale
-//    public LIFX()
-//    {
-//        apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
-//    }
+    public LIFX()
+    {
+        apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
+    }
 
     public JSONArray listLights()
     {
-        String apiKey = "ce6b4c1978015b537af377ac5792a923b9fe062f729d1558537d7e6bc7f9007f";
-
         try
         {
             URL adres = new URL("https://api.lifx.com/v1/lights/all");
             HttpsURLConnection polaczenie = (HttpsURLConnection) adres.openConnection();
             polaczenie.setRequestProperty("Authorization", "Bearer " + apiKey);
             polaczenie.setRequestMethod("GET");
+
+            System.out.println(polaczenie.getResponseCode());
 
             BufferedReader wejscie = new BufferedReader(new InputStreamReader(polaczenie.getInputStream()));
             StringBuilder odpowiedz = new StringBuilder();
